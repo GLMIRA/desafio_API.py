b5fbe951253bbe765b587e96c5ce702ed41f9db9
Gabriel Mira <gllimamira@gmail.com>
2024-06-20 13:33:55 -0300

    desafio_feito

---
+++ main.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/hello")
+def hello():
+    return "oi"
+
+
+@app.get("/raspador")
+def raspador():
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service)
+    browser.get("http://bianca.com")
+    conteudo = browser.find_element(By.TAG_NAME, "body").text
+    print(conteudo)
+    browser.close()
+    return conteudo
